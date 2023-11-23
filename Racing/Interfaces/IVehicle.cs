e9f2a572fa50cf090f57d68e58482cda2033e3ca
Yano4kaKulakova-PIE19 <ynakul@mail.ru>
2023-11-23 03:51:23 +0300

    first

---
+++ Racing/Interfaces/IVehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing.Interfaces;
+
+public interface IVehicle
+{
+    double Speed { get; set; }
+}
+
+public interface IGroundVehicle : IVehicle
+{
+    double DrivingTime { get; set; }
+
+    double GetRestTime(int idxStop);
+}
+
+public interface IAirVehicle : IVehicle
+{
+    double GetAcceleration(double traceDistance);
+}
